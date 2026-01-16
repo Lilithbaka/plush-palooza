86339428ff76c3262d46d876b7a6ce0cfd3283fc
Lilithbaka <juliamariadorosariosantos09@gmail.com>
2026-01-16 13:14:44 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/plush-palooza/',
   resolve: {
     alias: {
-      // Isso garante que o símbolo "@" aponte para a pasta src corretamente
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // Se o build falhar por causa de erros de TypeScript do Lovable, 
-  // adicione a linha abaixo para ignorar erros de TS no build e conseguir subir o site:
-  // build: { reportCompressedSize: false }
 })
