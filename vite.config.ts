3a2e2fae5555c172e1db0dd5a1f549a470236e9b
Lilithbaka <juliamariadorosariosantos09@gmail.com>
2026-01-16 13:04:43 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path" // Importação necessária para o resolve se usar o alias @
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/plush-palooza/', // ADICIONE ESTA LINHA COM O NOME DO SEU REPO
+  // Define a base para os assets (CSS, Imagens, JS) carregarem do lugar certo
+  base: '/plush-palooza/',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
